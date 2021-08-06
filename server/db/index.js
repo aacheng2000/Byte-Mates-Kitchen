2530cf7027d9ceb34042bee3ccc5142b41a68209
Zoe Liu <zliu27@u.rochester.edu>
2021-08-06 14:59:09 -0700

    Product Model complete, updated index.js to include Product associations with Fun and Theme

--- server/db/index.js
+++ server/db/index.js
@@ -1,14 +1,24 @@
 //this is the access point for all things database related!
 
-const db = require('./db')
+const db = require("./db");
 
-const User = require('./models/User')
+const User = require("./models/User");
+const Product = require("./models/Product");
+const { Fun, Status, Theme } = require("./models/Enumerations");
 
 //associations could go here!
+Fun.hasMany(Product);
+Product.belongsTo(Fun);
+Theme.hasMany(Product);
+Product.belongsTo(Theme);
 
 module.exports = {
   db,
   models: {
     User,
+    Product,
+    Fun,
+    Status,
+    Theme,
   },
-}
+};
