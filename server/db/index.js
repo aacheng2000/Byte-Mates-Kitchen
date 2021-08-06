9a0b92c39157a12f14be78e3fea556fb7a6e183d
Devin Boyd <djb91c@gmail.com>
2021-08-06 18:27:16 -0400

    added index.js

--- server/db/index.js
+++ server/db/index.js
@@ -3,12 +3,31 @@
 const db = require('./db')
 
 const User = require('./models/User')
+const Cart = require('./models/Cart')
+const Order = require('./models/Order')
+const Product = require('./models/Product')
+const { Fun, Status, Theme } = require("./models/Enumerations")
+
 
 //associations could go here!
+Cart.belongsTo(User)
+User.hasMany(Cart)
+
+Cart.hasMany(Order)
+Order.belongsTo(Cart)
+
+Cart.belongsTo(Status)
+Status.hasMany(Cart)
+
+Order.belongsTo(Product)
+Product.hasMany(Order)
 
 module.exports = {
   db,
   models: {
     User,
+    Cart,
+    Order,
+    Product
   },
 }
