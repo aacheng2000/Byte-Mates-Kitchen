ac8a161e6a45ec870fb3c6c9e98a244be3570358
djb91c <djb91c@gmail.com>
2021-08-07 11:24:24 -0400

    added all carts and user cart

--- server/api/carts.js
+++ server/api/carts.js
@@ -2,6 +2,7 @@ const router = require('express').Router()
 const { models: { Cart, User, Order, Status}} = require('../db')
 module.exports = router
 
+//shows all carts. need to make admin route to edit carts 
 router.get('/', async (req, res, next) => {
   try {
     const carts = await Cart.findAll({
@@ -11,4 +12,19 @@ router.get('/', async (req, res, next) => {
   } catch (err) {
     next(err)
   }
+})
+
+//shows the user their cart
+router.get('/:id', async (req, res, next) => {
+    try {
+        const userCart = await Cart.findAll({
+            where: {
+                userId: req.params.id,
+                statusId: 1
+            }
+        })
+        res.json(userCart)
+    } catch (error) {
+        next(err)
+    }
 })
