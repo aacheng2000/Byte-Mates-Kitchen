d85183dfb1edc5ce6b8ff22037228caa0c824c54
Zoe Liu <zliu27@u.rochester.edu>
2021-08-07 06:47:26 -0700

    added some dummy products

--- script/seed.js
+++ script/seed.js
@@ -1,29 +1,81 @@
-'use strict'
+"use strict";
 
-const {db, models: {User} } = require('../server/db')
+const {
+  db,
+  models: { User, Product },
+} = require("../server/db");
 
 /**
  * seed - this function clears the database, updates tables to
  *      match the models, and populates the database.
  */
 async function seed() {
-  await db.sync({ force: true }) // clears db and matches models to tables
-  console.log('db synced!')
+  await db.sync({ force: true }); // clears db and matches models to tables
+  console.log("db synced!");
 
   // Creating Users
   const users = await Promise.all([
-    User.create({ firstName: 'cody', lastName: 'snyder', email: 'snyder@gmail.com', phoneNumber: 1112223344, address: '1155 Big st.', password: '123', permission: 'admin' }),
-    User.create({ firstName: 'murphy', lastName: 'duncan', email: 'duncan@gmail.com', phoneNumber: 4443332211, address: '8877 Small st.', password: '123', permission: 'user' }),
-  ])
+    User.create({
+      firstName: "cody",
+      lastName: "snyder",
+      email: "snyder@gmail.com",
+      phoneNumber: 1112223344,
+      address: "1155 Big st.",
+      password: "123",
+      permission: "admin",
+    }),
+    User.create({
+      firstName: "murphy",
+      lastName: "duncan",
+      email: "duncan@gmail.com",
+      phoneNumber: 4443332211,
+      address: "8877 Small st.",
+      password: "123",
+      permission: "user",
+    }),
+  ]);
 
-  console.log(`seeded ${users.length} users`)
-  console.log(`seeded successfully`)
+  // Creating Products
+  const products = await Promise.all([
+    Product.create({
+      name: "Knive1",
+      description: "Great Knive 1",
+      price: 50.5,
+      color: "Black",
+      size: "Large",
+      picture: "https://picsum.photos/seed/picsum/200/300",
+    }),
+    Product.create({
+      name: "Fork1",
+      description: "Great Fork 1",
+      price: 10.5,
+      color: "Blue",
+      size: "Small",
+      picture: "https://picsum.photos/seed/picsum/200/300",
+    }),
+    Product.create({
+      name: "Spoon1",
+      description: "Great Spoon 1",
+      price: 20.5,
+      color: "Green",
+      size: "Large",
+      picture: "https://picsum.photos/seed/picsum/200/300",
+    }),
+  ]);
+
+  console.log(`seeded ${products.length} users`);
+  console.log(`seeded ${users.length} users`);
+  console.log(`seeded successfully`);
+
   return {
     users: {
       cody: users[0],
-      murphy: users[1]
-    }
-  }
+      murphy: users[1],
+    },
+    products: {
+      Knive,
+    },
+  };
 }
 
 /*
@@ -32,16 +84,16 @@ async function seed() {
  The `seed` function is concerned only with modifying the database.
 */
 async function runSeed() {
-  console.log('seeding...')
+  console.log("seeding...");
   try {
-    await seed()
+    await seed();
   } catch (err) {
-    console.error(err)
-    process.exitCode = 1
+    console.error(err);
+    process.exitCode = 1;
   } finally {
-    console.log('closing db connection')
-    await db.close()
-    console.log('db connection closed')
+    console.log("closing db connection");
+    await db.close();
+    console.log("db connection closed");
   }
 }
 
@@ -51,8 +103,8 @@ async function runSeed() {
   any errors that might occur inside of `seed`.
 */
 if (module === require.main) {
-  runSeed()
+  runSeed();
 }
 
 // we export the seed function for testing purposes (see `./seed.spec.js`)
-module.exports = seed
+module.exports = seed;
