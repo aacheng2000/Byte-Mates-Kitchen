c29bf9ce39b1946095edccc989beb0579d9cf3f8
EC2 Default User <ec2-user@ip-172-31-38-153.us-east-2.compute.internal>
2021-08-14 13:37:08 +0000

    hello

--- client/components/Home.js
+++ client/components/Home.js
@@ -1,8 +1,8 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import { connect } from "react-redux";
 import { HashRouter as Router, Route, Switch, Link } from "react-router-dom";
 import AllProducts from "./AllProducts";
-import {myCart} from '../store'
+import { myCart } from '../store'
 import AllUsers from "./AllUsers";
 
 
@@ -12,25 +12,57 @@ import AllUsers from "./AllUsers";
 
 
 
-class Home extends Component  {
-  componentDidMount(){
+class Home extends Component {
+  componentDidMount() {
     this.props.loadCartData(
       this.props.username
-      )
+    )
   }
 
-  render(){
-  const { username } = this.props;
-  const token = window.localStorage.getItem("token");
+  render() {
+    const { username } = this.props;
+    const token = window.localStorage.getItem("token");
 
 
-  return (
-    <div>
+    return (
+      <div>
       <h3>Welcome, {username}</h3>
       <AllUsers />
+    <table id="homeTable">
+      <tr id = "homeTableFirstRow">
+        <td>Knives</td>
+        <td>Forks</td>
+        <td>Spoons</td>
+      </tr>
+      <tr>
+        <td>
+          
+          <b>Contact us</b><br />
+          <b>Text: XXX-XXX-XXXX</b><br />
+          <b>Email:</b> <a href = "">xxx@bytemateskitchen.com</a><br />
+          Mon-Fri: 8am-7pm EST<br />
+          Sat-Sun: 8am-6pm EST<br />
+          
+        </td>
+        <b>Our Company</b><br />
+        About Us<br />
+        Careers<br />
+        Store Locations and Events<br />
+        <td>
+        <b>Social Media</b><br />
+        Show us your look with:<br />
+        #CrateStyle<br />
+        #CrateKidsStyle
+        </td>
+        <td>
+  
+        </td>
+      </tr>
+    </table>
     </div>
-  );
-};
+
+    );
+  };
 }
 
 /**
