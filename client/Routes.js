058c0b1b642571fd9622da878a42a14aa0ab36fc
Zoe Liu <zliu27@u.rochester.edu>
2021-08-16 14:25:47 -0700

    updated footer, functional components and backend routes for products with theme and functions

--- client/Routes.js
+++ client/Routes.js
@@ -5,6 +5,9 @@ import { Login, Signup } from "./components/AuthForm";
 import Home from "./components/Home";
 import Cart from "./components/Cart";
 import AllProducts from "./components/AllProducts";
+import AllForks from "./components/AllForks";
+import AllKnives from "./components/AllKnives";
+import AllSpoons from "./components/AllSpoons";
 import SingleProduct from "./components/SingleProduct";
 import { me, myCart } from "./store";
 import SingleUser from "./components/SingleUser";
@@ -28,9 +31,16 @@ class Routes extends Component {
             <Route path="/home" component={Home} />
             <Route path="/cart/:id" component={Cart} />
             <Route path="/products" exact component={AllProducts} />
+            <Route path="/category/forks" exact component={AllForks} />
+            <Route path="/category/knives" exact component={AllKnives} />
+            <Route path="/category/spoons" exact component={AllSpoons} />
             <Route exact path="/users/:id" component={SingleUser} />
             <Route exact path="/users/edit/:id" component={EditUser} />
-            <Route exact path="/products/:productId" component={SingleProduct} />
+            <Route
+              exact
+              path="/products/:productId"
+              component={SingleProduct}
+            />
             <Redirect to="/home" />
           </Switch>
         ) : (
@@ -39,7 +49,14 @@ class Routes extends Component {
             <Route path="/login" component={Login} />
             <Route path="/signup" component={Signup} />
             <Route exact path="/products" component={AllProducts} />
-            <Route exact path="/products/:productId" component={SingleProduct} />
+            <Route path="/category/forks" exact component={AllForks} />
+            <Route path="/category/knives" exact component={AllKnives} />
+            <Route path="/category/spoons" exact component={AllSpoons} />
+            <Route
+              exact
+              path="/products/:productId"
+              component={SingleProduct}
+            />
           </Switch>
         )}
       </div>
--- client/components/Home.js
+++ client/components/Home.js
@@ -1,8 +1,7 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { myCart } from '../store';
-import AllProducts from "./AllProducts";
-import Cart from "./Cart";
+import { Link } from "react-router-dom";
+import { myCart } from "../store";
 
 /**
  * COMPONENT
@@ -10,33 +9,37 @@ import Cart from "./Cart";
 
 class Home extends Component {
   componentDidMount() {
-    this.props.loadCartData(
-      this.props.username
-    )
+    this.props.loadCartData(this.props.username);
   }
 
   render() {
-    const { username } = this.props;
+    const { username, isLoggedIn } = this.props;
     const token = window.localStorage.getItem("token");
 
     return (
       <div>
-      <h3>Welcome, {username}</h3>
-
-      <table id="homeTableCategory">
-        <thead>
-          <tr id = "homeTableFirstRow">
-            <td>Knives               </td>
-            <td>Forks                </td>
-            <td>Spoons               </td>
-          </tr>
-        </thead>
-      </table>
+        <h3>Welcome, {username}</h3>
+        <div id="homeTableCategory">
+          <div id="homeTableFirstRow">
+            <Link to="/category/knives">All Knives</Link>
+            <Link to="/category/forks">All Forks</Link>
+            <Link to="/category/spoons">All Spoons</Link>
+          </div>
+        </div>
+        {/* 
+        <table id="homeTableCategory">
+          <thead>
+            <tr id="homeTableFirstRow">
+              <td>Knives </td>
+              <td>Forks </td>
+              <td>Spoons </td>
+            </tr>
+          </thead>
+        </table> */}
       </div>
     );
-  };
+  }
 }
-
 
 /**
  * CONTAINER
@@ -47,12 +50,12 @@ const mapState = (state) => {
   };
 };
 
-const mapDispatch = dispatch => {
+const mapDispatch = (dispatch) => {
   return {
     loadCartData(username) {
-      dispatch(myCart(username))
-    }
-  }
-}
+      dispatch(myCart(username));
+    },
+  };
+};
 
 export default connect(mapState, mapDispatch)(Home);
