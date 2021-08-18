411b98b34f26c73989fcb073ccb0d77016ea2f22
Zoe Liu <zliu27@u.rochester.edu>
2021-08-18 15:15:10 -0700

    add product functionality on product admin page

--- client/components/AddProduct.js
+++ client/components/AddProduct.js
@@ -44,61 +44,66 @@ class AddProduct extends Component {
     const { handleChange, handleSubmit } = this;
 
     return (
-      <form id="new-product-form" onSubmit={handleSubmit}>
-        <label htmlFor="name">Name: </label>
-        <input
-          value={name}
-          onChange={handleChange}
-          placeholder="New Product Name..."
-          name="name"
-        />
-        <label htmlFor="description">Description: </label>
-        <input
-          value={description}
-          onChange={this.handleChange}
-          placeholder="New Product Description..."
-          name="description"
-        />
-        <label htmlFor="price">Price: </label>
-        <input
-          value={price}
-          onChange={this.handleChange}
-          placeholder="New Product Price..."
-          name="price"
-        />
-        <label htmlFor="color">Color: </label>
-        <input
-          value={color}
-          onChange={this.handleChange}
-          placeholder="Color..."
-          name="color"
-        />
-        <label htmlFor="size">Size: </label>
-        <input
-          value={size}
-          onChange={this.handleChange}
-          placeholder="Size..."
-          name="size"
-        />
-        <label htmlFor="funId">Functional Category: </label>
-        <input
-          value={funId}
-          onChange={this.handleChange}
-          placeholder="Functional Category, 1, 2, or 3..."
-          name="funId"
-        />
-        <button type="submit">Add New Product</button>
-        <button>
-          <Link to="/adminproducts">Cancel</Link>
-        </button>
-      </form>
+      <div>
+        <div>
+          <h2>
+            <Link to="/adminproducts">All Products (Admin View)</Link>
+          </h2>
+        </div>
+        <div>
+          <form id="new-product-form" onSubmit={handleSubmit}>
+            <label htmlFor="name">Name: </label>
+            <input
+              value={name}
+              onChange={handleChange}
+              placeholder="New Product Name..."
+              name="name"
+            />
+            <label htmlFor="description">Description: </label>
+            <input
+              value={description}
+              onChange={this.handleChange}
+              placeholder="New Product Description..."
+              name="description"
+            />
+            <label htmlFor="price">Price: </label>
+            <input
+              value={price}
+              onChange={this.handleChange}
+              placeholder="New Product Price..."
+              name="price"
+            />
+            <label htmlFor="color">Color: </label>
+            <input
+              value={color}
+              onChange={this.handleChange}
+              placeholder="Color..."
+              name="color"
+            />
+            <label htmlFor="size">Size: </label>
+            <input
+              value={size}
+              onChange={this.handleChange}
+              placeholder="Size..."
+              name="size"
+            />
+            <label htmlFor="funId">Functional Category: </label>
+            <input
+              value={funId}
+              onChange={this.handleChange}
+              placeholder="Functional Category, 1, 2, or 3..."
+              name="funId"
+            />
+            <button type="submit">Add New Product</button>
+            <button>
+              <Link to="/adminproducts">Cancel</Link>
+            </button>
+          </form>
+        </div>
+      </div>
     );
   }
 }
 const mapDispatchToProps = { addNewProduct };
-
-// const mapDispatchToProps = (dispatch, { history }) => ({
-//   addNewProduct: (campus) => dispatch(addCampus(campus, history)),
-// });
 
 export default connect(null, mapDispatchToProps)(AddProduct);
