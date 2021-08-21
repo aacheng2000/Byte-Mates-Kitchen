02d1d78062d727572246ded6438da08f39a216be
Zoe Liu <zliu27@u.rochester.edu>
2021-08-21 07:17:51 -0700

    admin users css and more functionalities

--- client/components/SingleUser.js
+++ client/components/SingleUser.js
@@ -1,57 +1,50 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { fetchSingleUser } from '../store';
-import { Link } from 'react-router-dom';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { fetchSingleUser } from "../store";
+import { Link } from "react-router-dom";
 
 class SingleUser extends Component {
-    componentDidMount() {
-        this.props.loadUserData(this.props.match.params.id)
-    }
+  componentDidMount() {
+    this.props.loadUserData(this.props.match.params.id);
+  }
 
-    render() {
-        const { user } = this.props
-        
-        return (
-            <div>
-                <div className='single-user'>
-                    <div>
-                        Username: {user.username}
-                    </div>
-                    <div>
-                        First name: {user.firstName}
-                    </div>
-                    <div>
-                        Last name: {user.lastName}
-                    </div>
-                    <div>
-                        Email: {user.email}
-                    </div>
-                    <div>
-                        Phone number: {user.phoneNumber}
-                    </div>
-                    <div>
-                        Address: {user.address}
-                    </div>
-                </div>
+  render() {
+    const { user } = this.props;
 
-                <Link to={`edit/${user.id}`}>
-                    <button className='edit-btn'>Edit</button>
-                </Link>
-            </div>
-        )
-    }
-};
+    return (
+      <div>
+        <div className="single-user">
+          <div>Username: {user.username}</div>
+          <div>First name: {user.firstName}</div>
+          <div>Last name: {user.lastName}</div>
+          <div>Email: {user.email}</div>
+          <div>Phone number: {user.phoneNumber}</div>
+          <div>Address: {user.address}</div>
+        </div>
+
+        <Link to={`edit/${user.id}`}>
+          <button>Edit</button>
+        </Link>
+        <Link to={"/users"}>
+          <button>Back to Admin Users</button>
+        </Link>
+      </div>
+    );
+  }
+}
 
 const mapStateToProps = ({ user }) => {
-    return {
-        user
-    }
+  return {
+    user,
+  };
 };
 
-const mapDispatchToProps = (dispatch) => { 
-    return {
-       loadUserData: (id) => { dispatch(fetchSingleUser(id)) }
-    }
+const mapDispatchToProps = (dispatch) => {
+  return {
+    loadUserData: (id) => {
+      dispatch(fetchSingleUser(id));
+    },
+  };
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(SingleUser)
+export default connect(mapStateToProps, mapDispatchToProps)(SingleUser);
